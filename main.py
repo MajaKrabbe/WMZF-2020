7a36d1c83a75b081f196ebc1a22b0b71cd3e5ed5
Maja_Zdancewicz <joanneashen@gmail.com>
2020-12-06 14:12:45 +0100

    funkcje związane z dynamiką obiektu w polu centralnym: funkcje przyspieszenia grawitacyjnego, coriolisa, wypadkowego funkcje prędkości i położenia

--- main.py
+++ main.py
@@ -8,18 +8,11 @@ import numpy as np
 import constants as const
 import data_input as din
 import data_init as ini
-
-#todo: plik .dat z danymi dostępnych środowisk, wczytywanie go w temp_input().
-# format środowisk widzę tak:
-# name, gravfieldtype, surfacegrav, radius, rotation
-# name - bez polskich znaków
-# gravfieldtype - pole jednorodne czy centralne
-# surfacegrav - przyspieszenie graw. na powierzchni. z tego i promienia można policzyć masę ciała, kiedy jest ona potrzebna
-# radius - promień kuli, -1 dla pola jednorodnego (płaszczyzna)
-# rotation - prędkość kątowa obrotu środowiska. środowisko w wykresach chyba powinno być niezmienne, i wtedy pojawi się ten Coriolis
+import movement as mv
 
 def main():
     ini.dinit()
+    # din.data_input()
 
     return 0
 
