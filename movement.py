3f985d205f81de878d8e9a7ba143cf5ed43c4c2d
aleksandra-z <74962429+aleksandra-z@users.noreply.github.com>
2020-12-11 21:53:01 +0100

    Add files via upload

--- movement.py
+++ movement.py
@@ -1,106 +1,116 @@
+# -*- coding: utf-8 -*-
 import numpy as np
-import constants as const
 
-# todo: funkcje na ruch w polu jednorodnym
-
-def mass(sgrav:float, radius:float):
+def distance(pos_1, pos_2 = np.array([0,0,0])):
     '''
-    Oblicza masę ciała przy założeniu, że jest ono jednorodną kulą
+    Parameters
+    ----------
+    pos_1 : tablica, współżędne punktu wyrażone jako (x,y,z).
+    pos_2 : tablica, The default is np.array([0,0,0]).
 
-    :param sgrav: przyspieszenie grawitacyjne przy powierzchni ciała
-    :param radius: promień ciała
-    :returns: masa
+    Returns
+    -------
+    dist : float, odległosć między punktami.
     '''
-    return sgrav * (radius**2) / const.GRAV_CONST
+    squares = 0
+    vect = pos_1 - pos_2
+    for i in vect:
+        squares += i**2
+    dist = float(squares**(1/2))
+    return dist
 
-def vector_value(v:np.ndarray):
+def trajectoryPlasz(dt:float, acc:float, vel:np.array([]), 
+               pos = np.array([0, 0, 0])):
     '''
-    Oblicza długość danego wektora.
+    Parameters
+    ----------
+    dt : float, czas.
+    acc : float, przyspieszenie grawitacyjne.
+    vel : np.array([]), prędkosć początkowa.
+    pos : np.array([]), pozycja początkowa. The default is np.array([0, 0, 0]).
 
-    :param v: wektor
-    :return: długość wektora v
+    Returns
+    -------
+    r : np.array, położenie chwilowe.
     '''
-    val = 0.0
-    for crd in v:
-        val += float(crd)**2
-    val = val**0.5
-    return val
+    rx = vel[0]*dt + pos[0]
+    ry = vel[1]*dt + pos[1]
+    rz = vel[2]*dt + pos[2] + 0.5*dt**2*acc
+   
+    r = np.array([rx, ry, rz])
+    return r
 
-def gravityAcceleration(radius, x, y, z, surfaceGravAcceleration):
+def velocityPlasz(dt:float, acc:float, 
+                  vel = np.array([])):
     '''
-    obsolete ?
+    Parameters
+    ----------
+    dt : float, czas.
+    acc : float, przyspieszenie grawitacyjne.
+    vel : np.array([]), prędkosć początkowa.
+
+    Returns
+    -------
+    v : np.array([]), prędkosć chwilowa.
     '''
-    # r odległość w prostej linii punktu od powierzchni
-    r = (((radius - x) ** 2 + (radius - y) ** 2 + (radius - z) ** 2) ** (1 / 2))
-    #M = surfaceGravAcceleration * (radius ** 2) / const.GRAV_CONST
-    M = mass(surfaceGravAcceleration, radius)
+    vx = vel[0]
+    vy = vel[1]
+    vz = dt*acc + vel[2]
+    
+    v = np.array([vx, vy, vz])
+    return v
 
-    a = const.GRAV_CONST * M / (radius + r) ** 2
-    # To samo generalnie:
-    # g = (surfaceGravAcceleration/(1+(r/radius))**2)
-
-    return a
-
-def grav_acc(pos:np.ndarray, M:float):
+def timePlasz(acc:float, vel:np.array([]), 
+              pos = np.array([0, 0, 0])):
     '''
-    Oblicza współrzędne przyspieszenia chwilowego w zależności od wektora położenia względem masy M
+    Parameters
+    ----------
+    acc : float, przyspieszenie grawitacyjne.
+    vel : np.array([]), prędkosć początkowa.
+    pos : np.array([]), pozycja początkowa. The default is np.array([0, 0, 0]).
 
-    :param pos: wektor położenia obiektu
-    :param M: masa ciała przyciągającego obiekt, umieszczona w położeniu (0,0,0)
-    :return: acc - wektor przyspieszenia chwilowego
+    Returns
+    -------
+    t : float, czas całkowity.
     '''
-    try:
-        acc = pos * (-1) * const.GRAV_CONST * M / (vector_value(pos)**3)
-    except ZeroDivisionError:
-        acc = 0
-    return acc
+    delta = (-vel[2])**2 - 2*(-acc)*pos[2]
+    t = float((vel[2] - delta**(1/2))/-acc)
+    # t = czas całkowity ruchu
+    return t
 
-def velocity(dt:float, vel:np.ndarray = np.asarray([0,0,0]), acc:np.ndarray = np.asarray([0,0,0])):
+def maxHeight(acc:float, vel:np.array([]), 
+              pos = np.array([0, 0, 0])):
     '''
-    Oblicza prędkość obiektu poddanego przyspieszeniu w przedziale czasie dt
+    Parameters
+    ----------
+    acc : float, przyspieszenie grawitacyjne.
+    vel : np.array([]), prędkosć początkowa.
+    pos : np.array([]), pozycja początkowa. The default is np.array([0, 0, 0]).
 
-    :param dt: czas zmiany prędkości
-    :param acc: wektor przyspieszenia chwilowego
-    :param vel: prędkość początkowa
-    :return: nowa prędkość
+    Returns
+    -------
+    h : float, maksymalna wysokosć.
     '''
-    dv = dt * acc
-    return np.add(dv, vel)
+    h = vel[2]**2/2**acc + pos[2]
+    return h
 
-def position(dt:float, pos:np.ndarray = np.asarray([0,0,0]), vel:np.ndarray = np.asarray([0,0,0]), acc:np.ndarray = np.asarray([0,0,0])):
-    '''
-    Oblicza pozycję obiektu po upływie czasu dt
+# Nie do końca wiem co tutaj poniżej
 
-    :param dt: przedział czasu, w którym następuje przemieszczenie
-    :param acc: przyspieszenie chwilowe
-    :param vel: prędkość chwilowa
-    :param pos: położenie początkowe obiektu
-    :return: Nowa pozycja obiektu
+def reach(acc:float, t:float, vel:np.array([]), 
+          pos = np.array([0, 0, 0])):
     '''
-    dr = 0.5 * (dt**2) * acc + dt * vel
-    return np.add(dr, pos)
+    Parameters
+    ----------
+    acc : float, przyspieszenie grawitacyjne.
+    vel : np.array([]), prędkosć początkowa.
+    pos : np.array([]), pozycja początkowa. The default is np.array([0, 0, 0]).
+    t : float, czas całkowity ruchu.
 
-def coriolis_acc(v_linear:np.ndarray, v_radial:float):
+    Returns
+    -------
+    z : float, zasięg.
     '''
-    Zwraca przyspieszenie pozorne obiektu związane z ruchem obrotowym układu odniesienia wokół osi pionowej OZ
-
-    :param v_linear: wektor prędkości liniowej obiektu
-    :param v_radial: wartość prędkości kątowej układu odniesienia
-    :return: wektor przyspieszenia Coriolisa
-    '''
-    ax = 2 * v_radial * v_linear[1]
-    ay = -2 * v_radial * v_linear[0]
-    return np.asarray([ax,ay,0])
-
-def acceleration(pos:np.ndarray, M:float, v_linear:np.ndarray = np.asarray([0,0,0]), v_radial:float = 0):
-    '''
-    Zwraca wektor przyspieszenia wypadkowego obiektu poruszającego się w układzie odniesienia obracającym sie wokół osi OZ
-
-    :param pos:
-    :param M:
-    :param v_linear:
-    :param v_radial:
-    :return:
-    '''
-    acc = np.add( grav_acc(pos, M), coriolis_acc(v_linear, v_radial) )
-    return acc
+    v = (vel[0]**2-vel[1]**2)**(1/2)
+    z = v*t
+    
+    return z
