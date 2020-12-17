d929150dcdfafefe466d8bfefa6f74a74dae9d90
Maja_Zdancewicz <joanneashen@gmail.com>
2020-12-17 17:29:38 +0100

    obliczenia

--- main.py
+++ main.py
@@ -14,7 +14,44 @@ def main():
     ini.dinit()
     data = din.data_input()
 
-    #todo: obliczenia i wykresy
+    if data['grav_type'] == 'centralne':
+        if data['flag_rot'] == False:
+            data['v_rot'] = 0
+
+        dt = 0.02   # można by dodać dynamiczne zmienianie dt w zależności od wartości przyspieszenia,
+                    # im większe przyspieszenie tym większe dt ?
+
+        pos = []; pos.append(data['pos'])
+        acc = mv.acceleration(data['pos'], data['mass'], data['v_lin'], data['v_rot'])
+        vel = data['v_lin']
+
+        i = 0;
+        while mv.distance(pos[len(pos)-1]) >= data['radius']:
+            i += 1
+            pos.append(mv.position(dt, pos[i-1], vel, acc))
+            vel = mv.velocity(dt, vel, acc)
+            acc = mv.acceleration(pos[i], data['mass'], vel, data['v_rot'])
+
+        t = i * dt  # czas całkowity
+
+        #todo: wykres, przedstawienie danych
+
+    elif data['grav_type'] == 'jednorodne':
+        tmax = mv.timePlasz(data['surf_acc'], data['v_lin'], data['pos'])
+        steps = 5000
+        time = np.linspace(0, tmax, steps)
+
+        pos = np.zeros(steps+1)
+        #todo: zoptymalizować pętlę?
+        for step in range(0, steps+1):
+            pos[step] = mv.trajectoryPlasz(time[step], data['surf_acc'], data['v_lin'], data['pos'])
+
+        h_max = mv.maxHeight(data['surf_acc'], data['v_lin'], data['pos'])
+        h_max_test = pos[round(0.5*steps)]     # powinno być bliskie h_max
+
+        reach = mv.reach(data['surf_acc'], data['v_lin'], data['pos'])
+
+        # todo: wykres, przedstawienie danych
 
     return 0
 
