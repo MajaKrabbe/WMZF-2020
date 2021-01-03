e74aa43f71c389d0c1904bde9fee61ff64ac729d
Maja_Zdancewicz <joanneashen@gmail.com>
2021-01-03 16:11:28 +0100

    wyrównanie osi ox, oy

--- wykres.py
+++ wykres.py
@@ -5,8 +5,6 @@ import matplotlib.pyplot as plt
 
 def wykresCentr(poz:float, r:int):
     ax = plt.axes(projection='3d')
-    
-    
 
     argx = poz[:,0]
     argy = poz[:,1]
@@ -19,6 +17,14 @@ def wykresCentr(poz:float, r:int):
 #    z = np.linspace(argz, r)
     
     ax.plot3D(argx, argy, argz, 'r')
+
+    # wyrównanie skali osi x,y
+    #ax.set_box_aspect([1,1,1])
+    lmin = max(argx[0], argy[0], argz[0])
+    lmax = max(argx[len(argx)-1], argy[len(argy)-1], argz[len(argz)-1])
+    lmin, lmax, lmin, lmax= plt.axis()
+
+    plt.show()
     return 0
 
 def wykresPlaszcz(poz:np.ndarray):
@@ -29,5 +35,14 @@ def wykresPlaszcz(poz:np.ndarray):
     argz = poz[:,2]
 
     ax.plot3D(argx, argy, argz, 'r')
+
+    # wyrównanie skali osi x,y
+    # ax.set_box_aspect([1,1,1])
+    lmin = max(argx[0], argy[0], argz[0])
+    lmax = max(argx[len(argx) - 1], argy[len(argy) - 1], argz[len(argz) - 1])
+    lmin, lmax, lmin, lmax = plt.axis()
+
+    plt.show()
+
     return 0
 
